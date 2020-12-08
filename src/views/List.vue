2a00aa530bcbe0896adfdeb67353f5ff6a58ff80
仙女味的糖果 <2551940064@qq.com>
2020-12-08 15:28:08 +0800

    第二次提交

---
+++ src/views/List.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="weddingList">
+    <!-- 顶部 -->
+    <mt-header title="案例" fixed>
+      <mt-button icon="back" slot="left"></mt-button>
+    </mt-header>
+    <!-- 顶部选项卡开始 -->
+  </div>
+</template>
+<style>
+.weddingList>:first-child{
+  background-color: white;
+  color: #333;
+  font-size: 20px;
+}
+</style>

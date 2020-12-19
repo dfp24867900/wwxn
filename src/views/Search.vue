81df3e94e150ddf75326a36ffafa7b506c995152
jiezhezouyue <1031708506@qq.com>
2020-12-19 10:49:51 +0800

    du 12.19第二次上传

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -84,7 +84,7 @@ export default {
   text-align: center;
   background-color: rgba(0, 0, 0, 0);
   border: 0px;
-  color: #ffc0cb;
+  color: #000;
 }
 #search .header a > img {
   margin-top: 12px;
--- src/views/SearchList.vue
+++ src/views/SearchList.vue
@@ -153,7 +153,7 @@ export default {
   text-align: center;
   background-color: rgba(0, 0, 0, 0);
   border: 0px;
-  color: #ffc0cb;
+  color: #000;
 }
 #searchList .header a > img {
   margin-top: 12px;
